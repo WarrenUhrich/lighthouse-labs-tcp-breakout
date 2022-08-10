98158c5e9018959928aebe06f626f367b226a122
Warren <hello@warren.codes>
2022-08-10 15:34:41 -0600

    Add client accept string from user and send to server

--- 02-client.js
+++ 02-client.js
@@ -5,4 +5,16 @@ const client = net.createConnection({
     port: 54321
 });
 
-client.write('Hello, I am a client!');
+// client.write('Hello, I am a client!');
+
+client.setEncoding('utf-8');
+
+process.stdin.on('data', (data) => {
+    const enteredString = String(data).trim(); // Trim removes extra white space at the beginning / end of a string.
+    
+    // Print to client.
+    console.log('You entered:', enteredString);
+    
+    // Server will see written string.
+    client.write(enteredString); 
+});
